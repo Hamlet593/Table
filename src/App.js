70072ee1c00008ec6375311d0a908e8506d165ed
Hamo Muradyan <hamlet@advancedtech.am>
2023-05-29 18:27:26 +0400

    search function

--- src/App.js
+++ src/App.js
@@ -1,50 +1,115 @@
 import { useEffect, useState } from "react";
-import SearchBar from './SearchBar'
-import ListPage from './ListPage'
+import ListPage from "./ListPage";
 import axios from "axios";
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
+  const [searchId, setSearchId] = useState("");
+  const [searchName, setSearchName] = useState("");
+  const [searchDescription, setSearchDescription] = useState("");
 
-  const [posts, setPosts] = useState([])
-  const [searchResults, setSearchResults] = useState([])
+  const handleSearchChange = (e) => {
+    if (e.target.name === "id") {
+      setSearchId(e.target.value);
+      const resultsArray = users.filter((user) => {
+        return String(user.id).includes(e.target.value);
+      });
+      setSearchResult(resultsArray);
+    } else if (e.target.name === "name") {
+      setSearchName(e.target.value);
+      const resultsArray = users.filter((user) => {
+        return user.name.includes(e.target.value);
+      });
+      setSearchResult(resultsArray);
+    } else if (e.target.name === "description") {
+      setSearchDescription(e.target.value);
+      const resultsArray = users.filter((user) => {
+        return user.description.includes(e.target.value);
+      });
+      setSearchResult(resultsArray);
+    }
+  };
 
   const getData = async (url) => {
     try {
       const res = await axios.get(url);
       if (res.data.length > 0) {
         const newData = [];
-        res.data.forEach(item => {
-          newData.push({ id: item.id, name: item.name, description: item.company.bs.slice(0, 10) })
-        })
-        setPosts(newData)
-      };
+        res.data.forEach((item) => {
+          newData.push({
+            id: item.id,
+            name: item.name,
+            description: item.company.bs.slice(0, 10),
+          });
+        });
+        setUsers(newData);
+        setSearchResult(newData);
+      }
     } catch (e) {
-      alert('False fetch data')
+      alert("False fetch data");
     }
-  }
+  };
 
   useEffect(() => {
-    getData('https://jsonplaceholder.typicode.com/users');
-  }, [])
+    getData("https://jsonplaceholder.typicode.com/users");
+  }, []);
 
-  return <>
-    {/* <SearchBar posts={posts} setSearchResults={setSearchResults} /> */}
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Description</th>
-          <th>Date</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <ListPage posts={posts} searchResults={searchResults} setSearchResults={setSearchResults} />
-      </tbody>
-    </table>
-  </>
-}
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              ID
+              <div>
+                <input
+                  type="text"
+                  name="id"
+                  value={searchId}
+                  onChange={handleSearchChange}
+                  placeholder={`Search by id`}
+                />
+              </div>
+            </th>
+            <th>
+              Name
+              <div>
+                <input
+                  type="text"
+                  name="name"
+                  value={searchName}
+                  onChange={handleSearchChange}
+                  placeholder={`Search by name`}
+                />
+              </div>
+            </th>
+            <th>
+              Description
+              <div>
+                <input
+                  type="text"
+                  name="description"
+                  value={searchDescription}
+                  onChange={handleSearchChange}
+                  placeholder={`Search by description`}
+                />
+              </div>
+            </th>
+            <th>Date</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <ListPage
+            searchResult={searchResult}
+            setSearchResult={setSearchResult}
+            setUsers={setUsers}
+          />
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 export default App;
-
--- src/ListPage.js
+++ src/ListPage.js
@@ -1,40 +1,60 @@
+const ListPage = ({ searchResult, setSearchResult, setUsers }) => {
+  const handleChange = (e) => {
+    let { name, value, id } = e.target;
+    if (name === "id") value = +value;
+    const newObj = JSON.parse(JSON.stringify(searchResult));
+    newObj[id][name] = value;
+    setSearchResult(newObj);
+    setUsers(newObj);
+  };
 
-const ListPage = ({ posts, searchResults, setSearchResults }) => {
-
-    const handleChange = e => {
-        let { name, value, id } = e.target;
-        if (name === 'id') value = +value;
-        const newObj = JSON.parse(JSON.stringify(searchResults));
-        newObj[id][name] = value;
-        setSearchResults(newObj);
-    };
-
-    return posts.map((curUser, curIndex) => {
-        const { id, name, description } = curUser;
-        return (
-            <tr key={curIndex}>
-                <td>
-                    <input type="number" name='id' id={curIndex} value={id} onChange={handleChange} />
-                </td>
-                <td>
-                    <input type="text" name="name" id={curIndex} value={name} onChange={handleChange} />
-                </td>
-                <td>
-                    <textarea name='description' id={curIndex} value={description} onChange={handleChange} />
-                </td>
-                <td>
-                    <input type="date" name='date' id={curIndex} />
-                </td>
-                <td>
-                    <input list="options" id={curIndex} placeholder="Choose one..." />
-                    <datalist id="options" >
-                        <option value="Active" />
-                        <option value="Pernding" />
-                        <option value="Cancelled" />
-                    </datalist>
-                </td>
-            </tr>
-        )
-    });
-}
+  return searchResult.map((curUser, curIndex) => {
+    const { id, name, description } = curUser;
+    return (
+      <tr key={curIndex}>
+        <td>
+          <div>
+            <input
+              type="number"
+              name="id"
+              id={curIndex}
+              value={id}
+              onChange={handleChange}
+            />
+          </div>
+        </td>
+        <td>
+          <div>
+            <input
+              type="text"
+              name="name"
+              id={curIndex}
+              value={name}
+              onChange={handleChange}
+            />
+          </div>
+        </td>
+        <td>
+          <textarea
+            name="description"
+            id={curIndex}
+            value={description}
+            onChange={handleChange}
+          />
+        </td>
+        <td>
+          <input type="date" name="date" id={curIndex} />
+        </td>
+        <td>
+          <input list="options" id={curIndex} placeholder="Choose one..." />
+          <datalist id="options">
+            <option value="Active" />
+            <option value="Pernding" />
+            <option value="Cancelled" />
+          </datalist>
+        </td>
+      </tr>
+    );
+  });
+};
 export default ListPage;
