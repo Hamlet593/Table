c953c87efb2d9c5eea64d35189b1a85204f74a8e
Hamo Muradyan <hamomuradyan93@gmail.com>
2023-05-29 08:28:01 +0400

    resizible table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+// import { getPosts } from './api/axios'
+// import { useState, useEffect } from 'react'
+// import SearchBar from './SearchBar'
+// import ListPage from './ListPage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   const [posts, setPosts] = useState([])
+//   const [searchResults, setSearchResults] = useState([])
+
+//   useEffect(() => {
+//     getPosts().then(json => {
+//       setPosts(json)
+//       setSearchResults(json)
+//     })
+//   }, [])
+
+
+
+//   return (
+//     <>
+//       <SearchBar posts={posts} setSearchResults={setSearchResults} />
+//       <ListPage searchResults={searchResults} />
+//     </>
+//   )
+// }
+
+// export default App;
+
+import { useEffect, useState } from "react";
+import UserData from "./components/UserData.js";
+import axios from "axios";
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  const getData = async (url) => {
+    try {
+      const res = await axios.get(url);
+      if (res.data.length > 0) {
+        const newData = [];
+        res.data.forEach(item => {
+          newData.push({ id: item.id, name: item.name, description: item.company.bs.slice(0, 10) })
+        })
+        setUsers(newData)
+      };
+    } catch (e) {
+      alert('False fetch data')
+    }
+  }
+
+  useEffect(() => {
+    getData('https://jsonplaceholder.typicode.com/users');
+  }, [])
+
+  return <>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <UserData users={users} setUsers={setUsers} />
+      </tbody>
+    </table>
+  </>
 }
 
 export default App;
+
